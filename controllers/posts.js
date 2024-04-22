0c7e17bbf20ea7bccf59d8f8ce976f0428b1a979
Taylor Stallcup <tstallcup321@gmail.com>
2024-04-22 16:06:32 -0500

    Create Post successful

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,26 @@
+const post = require('../models/post');
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports = {
     index,
+    create,
+}
+
+async function create(req, res) {
+    const post = new Post(req.body);
+    // assign the logged in user's id
+    post.author = req.user._id;
+    console.log('this is a post: ',post);
+    console.log('author: ', post.author);
+    try {
+        await post.save();
+        res.redirect('posts');
+    } catch(error) {
+        console.log('Error creating a post: ', error);
+    }
+
+    // console.log('this is the content: ', content)
 }
 
 async function index(req, res) {
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,7 @@ const postsCtrl = require('../controllers/posts');
 
 /* GET users listing. */
 router.get('/', postsCtrl.index);
+// GET /posts/new
+router.post('/', postsCtrl.create);
 
 module.exports = router;
