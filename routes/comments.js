3f9daf34c63cad961ad1ed95619c0f5537bd9f99
Taylor Stallcup <tstallcup321@gmail.com>
2024-04-24 14:46:21 -0500

    Deploy to Heroku

--- routes/comments.js
+++ routes/comments.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 
-//* ALL ROUTES START WITH '/posts'
-// 
+//* ALL ROUTES START WITH '/' (was starting with /posts)
+// POST /posts/:postId/comments (Create a comment on a post)
 
 
 
--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(function (req, res, next) {
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/users', usersRouter);
-app.use('posts', commentsRouter);
+app.use('/', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
